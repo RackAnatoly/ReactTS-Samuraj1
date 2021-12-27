f29fb8b4366c42bb701147de85e576dd256995d9
Anatoli Rak <Rack.anatoly@gmail.com>
2021-12-27 10:53:05 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hi man!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Message</a>
+        </div>
+    )
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>rect</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;

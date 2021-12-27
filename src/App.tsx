3cf5e0226414ca6d90261dc45b25336fca5a9555
RackAnatoly <82027973+RackAnatoly@users.noreply.github.com>
2021-12-27 10:09:38 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      hi
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

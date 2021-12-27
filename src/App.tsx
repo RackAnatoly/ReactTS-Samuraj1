d4012c3c8676543ce53e0e944f44e23fc7082b66
Anatoli Rak <rack.anatoly@gmail.com>
2021-12-27 10:12:34 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hi
+      hi man
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

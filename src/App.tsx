1556ec147ad7cda5e8a565023b036c485c7c5bbf
Anatoli Rak <Rack.anatoly@gmail.com>
2022-01-11 22:39:11 +0100

    lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className="header">
+                <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_3.png" alt=""/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a href="">Profile</a>
+                </div>
+                <div>
+                    <a href="">Message</a>
+                </div>
+                <div>
+                    <a href="">News</a>
+                </div>
+                <div>
+                    <a href="">Music</a>
+                </div>
+                <div>
+                    <a href="">Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://www.w3schools.com/css/img_5terre_wide.jpg" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
-
-
 
 
 export default App;

b57b8b8357acba04361fe083327af5f0b1fa9f38
RackAnatoly <82027973+RackAnatoly@users.noreply.github.com>
2022-01-15 20:15:34 +0100

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './Header.module.css'
 
-const Header = () => {
+const Header = (props:any) => {
     return (
         <header className={s.header}>
             <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_3.png" alt=""/>

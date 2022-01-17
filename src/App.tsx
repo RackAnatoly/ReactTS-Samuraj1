68ccf012b44cd51ad9bedb87bbbc1455e283e84b
Anatoli Rak <Rack.anatoly@gmail.com>
2022-01-17 21:39:55 +0100

    lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 function App() {
@@ -11,7 +12,10 @@ function App() {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+            </div>
+            {/*<Profile/>*/}
         </div>
     );
 }
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Dialogs.module.css'
+
+export const Dialogs = () => {
+    return (
+        <div className={s.content}>
+            Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;

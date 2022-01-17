36af0c3306d7f25640108f51f4c41fcd16bfbda4
Anatoli Rak <Rack.anatoly@gmail.com>
2022-01-17 23:13:46 +0100

    lesson 20

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/message' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                    </Routes>
+                </div>
             </div>
-            {/*<Profile/>*/}
-        </div>
-    );
+        </BrowserRouter>
+    )
 }
 
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,20 +5,20 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             {/*<div className={`${s.item} ${s.active}`}>*/}
             <div className={s.item}>
-                <a>Message</a>
+                <a href="/message">Message</a>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <a href="news">News</a>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <a href="music">Music</a>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <a href="settings">Settings</a>
             </div>
         </nav>
     );

5cedfc5e74a27e28fcea7a278edc597e57ea3a39
Anatoli Rak <Rack.anatoly@gmail.com>
2022-01-18 12:50:39 +0100

    lesson 20

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,20 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>Anatoli</div>
+                <div className={s.dialog}>Tatsi</div>
+                <div className={s.dialog}>Adrian</div>
+                <div className={s.dialog}>Alex</div>
+                <div className={s.dialog}>Piotr</div>
+                <div className={s.dialog}>Sasha</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How Are you</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
